b4b68e49af84a9ca9831a7fdc5e6b16f74ba1bf1
David Leib <david.leib@barzahlen.de>
2016-04-15 09:46:37 +0200

    Externalized signature creation to make it externaly usable

--- lib/barzahlen_v2/signature.rb
+++ lib/barzahlen_v2/signature.rb
@@ -14,15 +14,20 @@ module BarzahlenV2
         request_method = method.upcase
         request_host_path = URI.parse(request_uri).path
         request_query_string = URI.encode_www_form(params)
-        request_idempotency_key = @settings[0] if @settings
+        request_idempotency_key = opts[:headers]["Idempotency-Key"]
         # Prepare the Date header
         request_date_header = DateTime.new.strftime("%a, %d %b %Y %H:%M:%S %Z")
 
-        request_body_digest = OpenSSL::Digest.digest("SHA256", body.to_s)
-
-        signature = "#{request_host_header}\n#{request_method}\n#{request_host_path}\n#{request_query_string}\n#{request_date_header}\n#{request_idempotency_key}\n#{request_body_digest}"
-
-        signature = OpenSSL::HMAC.hexdigest("SHA256", BarzahlenV2.configuration.payment_key, signature)
+        signature = BarzahlenV2::Middleware.generate_bz_signature(
+          request_host_header,
+          request_method,
+          BarzahlenV2::configuration.payment_key,
+          request_date_header,
+          request_host_path,
+          request_query_string,
+          body,
+          request_idempotency_key
+          )
 
         # Attach the Date, Authorization and Host to the request
         signature_headers = {
@@ -33,14 +38,24 @@ module BarzahlenV2
             }
           }
 
-        if request_idempotency_key
-          signature_headers[:headers]["Idempotency-Key"] = request_idempotency_key
-        end
-
         result = @request.call(opts[:headers].merge(signature_headers), request_uri, method, params, body)
 
         return result
       end
+    end
+
+    def self.generate_bz_signature(request_host_header, request_method, payment_key, request_date_header,
+      request_host_path = "", request_query_string = "", request_body = "", request_idempotency_key = "")
+      request_body_digest = BarzahlenV2::Middleware.generate_sha256_digest(request_body)
+
+      raw_signature = "#{request_host_header}\n#{request_method}\n#{request_host_path}\n#{request_query_string}\n"\
+        "#{request_date_header}\n#{request_idempotency_key}\n#{request_body_digest}"
+
+      OpenSSL::HMAC.hexdigest("SHA256", payment_key, raw_signature)
+    end
+
+    def self.generate_sha256_digest(string_to_hash)
+      request_body_digest = OpenSSL::Digest.digest("SHA256", string_to_hash.to_s)
     end
   end
 end
