b6f778d98971e946f1877aae982b1da021aff849
David Leib <david.leib@barzahlen.de>
2016-04-18 22:29:53 +0200

    First attempt of the client lib core

---
+++ lib/barzahlen_v2/online.rb
@@ -0,0 +1,140 @@
+require "securerandom"
+require "uri"
+require "json"
+
+module BarzahlenV2
+  module Online
+    IDEMPOTENCY_ENABLED = true
+
+    #For idempotency purposes a class takes care of refund and payment
+
+    class Slip
+      def initialize(opts = {})
+        if !opts[:slip_type]
+          raise BarzahlenV2::Error::ArgumentMissing.new(["slip_type"])
+        end
+
+        if opts[:slip_type].eql? "refund"
+          begin
+            if !opts[:refund][:for_slip_id] || !opts[:transactions][0][:amount] || \
+              !opts[:transactions][0][:currency]
+              raise BarzahlenV2::Error::ArgumentMissing.new(["for_slip_id","transactions > amount",
+                "transactions > currency"])
+            end
+          rescue NoMethodError
+            raise BarzahlenV2::Error::ArgumentMissing.new(["for_slip_id","transactions > amount",
+              "transactions > currency"])
+          end
+        elsif opts[:slip_type].eql? "payment"
+          begin
+            if !opts[:customer][:key] || !opts[:transactions][0][:amount] || \
+              !opts[:transactions][0][:currency]
+              raise BarzahlenV2::Error::ArgumentMissing.new(["customer > key","transactions > amount",
+                                                             "transactions > currency"])
+            end
+          rescue NoMethodError
+            raise BarzahlenV2::Error::ArgumentMissing.new(["customer > key","transactions > amount",
+                                                           "transactions > currency"])
+          end
+        else
+          raise BarzahlenV2::Error::ArgumentMissing.new(["slip_type"])
+        end
+
+        @request = BarzahlenV2::Online.get_grac_client(BarzahlenV2::Online::IDEMPOTENCY_ENABLED)
+        @request_hash = opts
+      end
+
+      def create
+        @request_hash.freeze
+        @request_hash.each do |key, value|
+          @request_hash[key].freeze
+        end
+        @request.path("/slips").post(@request_hash)
+      end
+    end
+
+    #If idempotency is not important a simple request is more than enough
+
+    def self.retrieve_slip(slip_id)
+      if !slip_id
+        raise BarzahlenV2::Error::ArgumentMissing.new(["slip_id"])
+      end
+
+      self.get_grac_client.path("/slips/#{slip_id}").get
+    end
+
+    def self.update_slip(slip_id, opts = {})
+      if !slip_id
+        raise BarzahlenV2::Error::ArgumentMissing.new(["slip_id"])
+      end
+      self.get_grac_client.path("/slips/#{slip_id}").patch(opts)
+    end
+
+    def self.resend_slip(slip_id, message_type)
+      if !slip_id || !message_type
+        raise BarzahlenV2::Error::ArgumentMissing.new(["slip_id","message_type"])
+      end
+      self.get_grac_client.path("/slips/#{opts[:id]}/resend/#{opts[:message_type]}").post
+    end
+
+    def self.invalidate_slip(slip_id)
+      if !slip_id
+        raise BarzahlenV2::Error::ArgumentMissing.new(["slip_id"])
+      end
+      self.get_grac_client.path("slips/#{slip_id}/invalidate")
+    end
+
+    # Handle a webhook request
+
+    def self.webhook_request(response)
+      bz_hook_format = response.headers["Bz-Hook-Format"]
+
+      #stop processing when bz-hook-format = v1 because it will be send as v2 again
+      if bz_hook_format.include? "v1"
+        return nil
+      end
+
+      content_type = response.headers["Content-Type"]
+
+      if ! content_type.include? "application/json"
+        return nil
+      end
+
+      bz_signature = response.headers["Bz-Signature"]
+
+      signature = BarzahlenV2::Middleware.generate_bz_signature(
+        response.headers["Host"],
+        "POST",
+        response.headers["Date"],
+        response.location,
+        "",
+        response.body
+        )
+
+      if bz_signature.include? signature
+        return JSON.parse(response.body)
+      else
+        raise BarzahlenV2::Error::SignatureError.new("Signature not valid")
+      end
+    end
+
+    private
+      @@grac_client = nil
+
+      def self.get_grac_client(idempotency = false)
+        if !@@grac_client
+          @@grac_client = Grac::Client.new(
+            BarzahlenV2.configuration.sandbox ? BarzahlenV2::Configuration::API_HOST_SANDBOX :
+              BarzahlenV2::Configuration::API_HOST,
+            middleware: [BarzahlenV2::Middleware::Signature]
+            )
+        end
+
+        if idempotency
+          return @@grac_client.set( headers: { "Idempotency-Key" => SecureRandom.uuid} )
+        else
+          return @@grac_client
+        end
+      end
+  end
+end
---
+++ spec/lib/online_spec.rb
@@ -0,0 +1,197 @@
+require "spec_helper"
+
+module BarzahlenV2
+  module Online
+    describe Slip do
+      let(:refund_slip) {
+        return {
+            slip_type: "refund",
+            refund: {
+              for_slip_id: "slp-d90ab05c-69f2-4e87-9972-97b3275a0ccd"
+            },
+            transactions: [
+              {
+                currency: "EUR",
+                amount: "-23.99"
+              }
+            ]
+          }
+      }
+
+      let(:payment_slip) {
+        return {
+            slip_type: "payment",
+            customer: {
+              key: "ASDFJKL"
+            },
+            transactions: [
+              {
+                currency: "EUR",
+                amount: "321.47"
+              }
+            ]
+          }
+      }
+
+      after :each do
+        @@grac_client = nil
+      end
+
+      it "generates a refund with idempotency header" do
+        request_client = double("Dummy Request Client")
+        expect(request_client).to receive(:set)
+        expect(Grac::Client).to receive(:new).and_return(request_client)
+        expect {
+          BarzahlenV2::Online::Slip.new(refund_slip)
+          }.to_not raise_error
+      end
+
+      it "generates a payment with idempotency header" do
+        request_client = double("Dummy Request Client")
+        expect(request_client).to receive(:set)
+        expect(Grac::Client).to receive(:new).and_return(request_client)
+        expect {
+          BarzahlenV2::Online::Slip.new(payment_slip)
+          }.to_not raise_error
+      end
+
+      it "payment fails if no slip type is specified" do
+        payment_slip.delete(:slip_type)
+        expect {
+            BarzahlenV2::Online::Slip.new(payment_slip)
+          }.to raise_error { |error|
+            expect(error.message).to eq("One or all MissingArgument(s): 'slip_type'")
+          }
+      end
+
+      it "payment fails if no customer is specified" do
+        payment_slip.delete(:customer)
+        expect {
+            BarzahlenV2::Online::Slip.new(payment_slip)
+          }.to raise_error { |error|
+            expect(error.message).to eq("One or all MissingArgument(s): 'customer > key','transactions > amount','transactions > currency'")
+          }
+      end
+
+      it "payment fails if no transaction is specified" do
+        payment_slip.delete(:transactions)
+        expect {
+            BarzahlenV2::Online::Slip.new(payment_slip)
+          }.to raise_error { |error|
+            expect(error.message).to eq("One or all MissingArgument(s): 'customer > key','transactions > amount','transactions > currency'")
+          }
+      end
+
+      it "refund fails if no slip_type is specified" do
+        refund_slip.delete(:slip_type)
+        expect {
+            BarzahlenV2::Online::Slip.new(refund_slip)
+          }.to raise_error { |error|
+            expect(error.message).to eq("One or all MissingArgument(s): 'slip_type'")
+          }
+      end
+
+      it "refund fails if no refund is specified" do
+        refund_slip.delete(:refund)
+        expect {
+            BarzahlenV2::Online::Slip.new(refund_slip)
+          }.to raise_error { |error|
+            expect(error.message).to eq("One or all MissingArgument(s): 'for_slip_id','transactions > amount','transactions > currency'")
+          }
+      end
+
+      it "refund fails if no transaction is specified" do
+        refund_slip.delete(:transactions)
+        expect {
+            BarzahlenV2::Online::Slip.new(refund_slip)
+          }.to raise_error { |error|
+            expect(error.message).to eq("One or all MissingArgument(s): 'for_slip_id','transactions > amount','transactions > currency'")
+          }
+      end
+
+      it "is setting correct uri" do
+        request = BarzahlenV2::Online::Slip.new(refund_slip)
+
+        expect(@@grac_client.uri).to eq("https://api.barzahlen.de/v2")
+      end
+
+      it "is setting correct sandbox uri" do
+        BarzahlenV2.configure { |config|
+          config.sandbox = true
+        }
+
+        request = BarzahlenV2::Online::Slip.new(refund_slip)
+
+        expect(@@grac_client.uri).to eq("https://api-sandbox.barzahlen.de/v2")
+      end
+
+      it "generates a payment and creates request with grac" do
+        request_client = double("Dummy Request Client")
+        request_client_with_idempotency = double("Dummy Idempotency Request Client")
+        expect(request_client_with_idempotency).to receive(:path).and_return(request_client_with_idempotency)
+        expect(request_client_with_idempotency).to receive(:post).with(payment_slip)
+        expect(request_client).to receive(:set).and_return(request_client_with_idempotency)
+        expect(Grac::Client).to receive(:new).and_return(request_client)
+        new_payment_slip = nil
+        expect {
+          new_payment_slip = BarzahlenV2::Online::Slip.new(payment_slip)
+          }.to_not raise_error
+        expect {
+          new_payment_slip.create
+          }.to_not raise_error
+      end
+    end
+
+    describe "Retrieving a slip" do
+      it "does not accept nil slip_id" do
+        expect {
+            BarzahlenV2::Online.retrieve_slip(nil)
+          }.to raise_error { |error|
+            expect(error.message).to eq("One or all MissingArgument(s): 'slip_id'")
+          }
+      end
+    end
+
+    describe "Updating a slip" do
+      it "does not accept nil slip_id" do
+        expect {
+            BarzahlenV2::Online.update_slip(nil,{})
+          }.to raise_error { |error|
+            expect(error.message).to eq("One or all MissingArgument(s): 'slip_id'")
+          }
+      end
+    end
+
+    describe "Resend a slip" do
+      it "does not accept nil slip_id" do
+        expect {
+            BarzahlenV2::Online.resend_slip(nil,"email")
+          }.to raise_error { |error|
+            expect(error.message).to eq("One or all MissingArgument(s): 'slip_id','message_type'")
+          }
+      end
+
+      it "does not accept nil message_type" do
+        expect {
+            BarzahlenV2::Online.resend_slip(nil,"email")
+          }.to raise_error { |error|
+            expect(error.message).to eq("One or all MissingArgument(s): 'slip_id','message_type'")
+          }
+      end
+    end
+
+    describe "Invalidate a slip" do
+      it "does not accept nil slip_id" do
+        expect {
+            BarzahlenV2::Online.invalidate_slip(nil)
+          }.to raise_error { |error|
+            expect(error.message).to eq("One or all MissingArgument(s): 'slip_id'")
+          }
+      end
+    end
+
+    describe "Webhook request" do
+      #need to test
+    end
+  end
+end
